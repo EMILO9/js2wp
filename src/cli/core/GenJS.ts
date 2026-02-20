43aedb194cf20938e19c80103a46ee010fd77aa5
Emil Berberg Olsen <emil@NETT-OPP.local>
2026-02-20 15:52:41 +0100

    -

--- src/cli/core/GenJS.ts
+++ src/cli/core/GenJS.ts
@@ -4,27 +4,29 @@ import { build } from "vite";
 import fg from "fast-glob";
 
 export async function GenJS(config: ConfigResult) {
-	const { computed: c, build: b } = config;
-	const entry = b.entry
-		? b.entry
-		: ((
-				await fg(["src/{index,main}.{ts,js}", "{index,main}.{ts,js}"])
-			)[0] ?? "src/index.ts");
-	await build({
-		root: process.cwd(),
-		logLevel: "silent",
-		build: {
-			outDir: path.join(process.cwd(), ".plugins", c.slug_kebab),
-			emptyOutDir: false,
-			lib: {
-				entry: entry,
-				name: c.slug_constant,
-				formats: ["iife"],
-				fileName: () => `assets/js/${c.slug_kebab}.js`,
-				cssFileName: `assets/css/${c.slug_kebab}`,
-			},
-			cssCodeSplit: false,
-			assetsDir: "assets",
-		},
-	});
+  const { computed: c, build: b } = config;
+  const entry = b.entry
+    ? b.entry
+    : ((await fg(["src/{index,main}.{ts,js}", "{index,main}.{ts,js}"]))[0] ??
+      "src/index.ts");
+  await build({
+    root: process.cwd(),
+    logLevel: "silent",
+    build: {
+      rollupOptions: { external: ["@wordpress/i18n"] },
+      outDir: path.join(process.cwd(), ".plugins", c.slug_kebab),
+      emptyOutDir: false,
+      minify: false,
+      lib: {
+        entry: entry,
+        name: c.slug_constant,
+        formats: ["iife"],
+        fileName: () => `assets/js/${c.slug_kebab}.js`,
+        cssFileName: `assets/css/${c.slug_kebab}`,
+      },
+      cssCodeSplit: false,
+      assetsDir: "assets",
+    },
+    esbuild: { keepNames: true },
+  });
 }
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -14,63 +14,57 @@ import { remove } from "fs-extra";
 const cliName = parsePackageName(pkg.name).fullName;
 const program = new Command();
 const explorer = cosmiconfig(cliName, {
-	loaders: {
-		".ts": TypeScriptLoader(),
-	},
+  loaders: {
+    ".ts": TypeScriptLoader(),
+  },
 });
 
 program.name(cliName).description(pkg.description).version(pkg.version);
 
 program
-	.command("build")
-	.description("...")
-	.action(async () => {
-		const rawConfig = await explorer.search();
-		const config = await GetConfig(rawConfig);
-		const { computed: c } = config;
-
-		const tasks = new Listr([
-			{ title: " 🧹 Cleaning old build", task: () => remove(".plugins") },
-			{
-				title: " 💻 Building plugin code",
-				task: () =>
-					new Listr([
-						{
-							title: " ⚡ Generating PHP files",
-							task: () => GenPHP(config),
-						},
-						{
-							title: " ✨ Generating JS files",
-							task: () => GenJS(config),
-						},
-					]),
-			},
-			{
-				title: " 🌐 Generating translation files",
-				task: () =>
-					new Listr([
-						{
-							title: "📄 POT for PHP",
-							task: () =>
-								execa("npx", [
-									"makepot",
-									`.plugins/${c.slug_kebab}`,
-									`.plugins/${c.slug_kebab}/languages`,
-								]),
-						},
-						{
-							title: "📝 POT for JS",
-							task: () =>
-								execa("npx", [
-									"makepot",
-									"src",
-									"js-translations",
-								]),
-						},
-					]),
-			},
-		]);
-		await tasks.run();
-	});
+  .command("build")
+  .description("...")
+  .action(async () => {
+    const rawConfig = await explorer.search();
+    const config = await GetConfig(rawConfig);
+    const { computed: c } = config;
+    const tasks = new Listr([
+      { title: " 🧹 Cleaning old build", task: () => remove(".plugins") },
+      {
+        title: " 💻 Building plugin code",
+        task: () =>
+          new Listr([
+            {
+              title: " ⚡ Generating PHP files",
+              task: () => GenPHP(config),
+            },
+            {
+              title: " ✨ Generating JS files",
+              task: () => GenJS(config),
+            },
+          ]),
+      },
+      {
+        title: " 🌐 Generating translation files",
+        task: () =>
+          execa(
+            "makepot",
+            [
+              `.plugins/${c.slug_kebab}`,
+              `.plugins/${c.slug_kebab}/languages`,
+              "--json",
+              "--silent",
+            ],
+            {
+              preferLocal: true,
+              cwd: process.cwd(),
+              stdio: "inherit",
+              shell: true,
+            },
+          ),
+      },
+    ]);
+    await tasks.run();
+  });
 
 program.parse();
