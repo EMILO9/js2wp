6550c5d06cabf4eba314473d8e8bbd3c3ffd074b
EMILO9 <emilberolsen@gmail.com>
2026-02-24 01:56:51 +0100

    d

--- src/cli/core/GenJS.ts
+++ src/cli/core/GenJS.ts
@@ -4,30 +4,31 @@ import { build } from "vite";
 import fg from "fast-glob";
 
 export async function GenJS(config: ConfigResult) {
-  const { computed: c, build: b } = config;
-  const entry = b.entry
-    ? b.entry
-    : ((await fg(["src/{index,main}.{ts,js}", "{index,main}.{ts,js}"]))[0] ??
-      "src/index.ts");
-  await build({
-    root: process.cwd(),
-    logLevel: "silent",
-    build: {
-      rollupOptions: { external: ["@wordpress/i18n"] },
-      outDir: path.join(process.cwd(), ".plugins", c.slug_kebab),
-      emptyOutDir: false,
-      minify: false,
-      lib: {
-        entry: entry,
-        name: c.slug_constant,
-        formats: ["iife"],
-        fileName: () => `assets/js/${c.slug_kebab}.js`,
-        cssFileName: `assets/css/${c.slug_kebab}`,
-      },
-      assetsInlineLimit: 0,
-      cssCodeSplit: true,
-      assetsDir: "assets",
-    },
-    esbuild: { keepNames: true },
-  });
+	const { computed: c, build: b } = config;
+	const entry = b.entry
+		? b.entry
+		: ((
+				await fg(["src/{index,main}.{ts,js}", "{index,main}.{ts,js}"])
+			)[0] ?? "src/index.ts");
+	await build({
+		root: process.cwd(),
+		logLevel: "silent",
+		build: {
+			rollupOptions: { external: ["@wordpress/i18n"] },
+			outDir: path.join(process.cwd(), ".plugins", c.slug_kebab),
+			emptyOutDir: false,
+			minify: false,
+			lib: {
+				entry: entry,
+				name: c.slug_constant,
+				formats: ["iife"],
+				fileName: () => `js/${c.slug_kebab}.js`,
+				cssFileName: `css/${c.slug_kebab}`,
+			},
+			assetsInlineLimit: 0,
+			cssCodeSplit: true,
+			assetsDir: "assets",
+		},
+		esbuild: { keepNames: true },
+	});
 }
